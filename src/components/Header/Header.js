e131e0f94f9d3f3f48901dd145eee81822280c75
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-01-23 14:52:16 +0300

    navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
-import { Flex } from "antd";
+import  Flex  from "../designLayouts/Flex";
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, NavLink, useLocation } from "react-router-dom"
+import logo from "../../assets/images/logo.png"
+import Image from "../designLayouts/Image"
+import { motion } from "framer-motion";
 
 
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(true);
-
+    const [sidenav, setSidenav] = useState(false);
+    const location = useLocation();
     useEffect(() => {
         let ResponsiveMenu = () => {
             if (window.innerWidth < 667) {
@@ -25,9 +29,45 @@ const Header = () => {
         <Flex className="flex items-center justify-between h-full">
         <Link to="/">
             <div>
-
+             <Image className="w-20 object-cover" imgSrc={logo} />
             </div>
         </Link>
+        <div>
+          {showMenu && (
+            <motion.ul
+             initial={{ y: 30, opacity: 0}}
+             animate={{ y: 0, opacity: 1}}
+             transition={{ duration: 0.5}}
+             className="flex items-center w-auto z-50 p-0 gap-2">
+
+                <>
+                {navBarList.map(({ _id,title, link }) => (
+                    <NavLink key={_id} className="flex font-normal hover:font-bold w-20 h-6 justify-center items-center px-12 text-base text-[#767676] hover:underline underline-offset-[4px] decoration-[1px] hover:text-[#262626] md:border-r-[2px] border-r-gray-300 hoverEffect last:border-r-0" to={link} state={{ data: location.pathname.split("/")[1]}}> 
+                    <li>{title}</li> 
+                    </NavLink>
+                ))}
+                </>
+            </motion.ul>
+          )}
+          <HiMenuAlt2 onClick={() => setSidenav(!sidenav)} className="inline-block md:hidden cursor-pointer w-8 h-6 absolute top-6 right-4"/>
+            {sidenav && (
+                <div className="fixed top-0 left-0 w-full h-screen bg-black text-gray-200 bg-opacity-80 z-50">
+                <motion.div initial={{ x: -300, opacity: 0}} animate={{ x: 0, opacity: 1}}
+                transition={{ duration: 0.5}} className="w-[80%] h-full relative">
+                <div className="w-full h-full bg-primeColor p-6">
+                <img className="w-28 mb-6" src={logo} alt="logoLight"/>
+                <ul className="text-gray-20 0 flex flex-col gap-2">
+                    {navBarList.map((item) => (
+                       <li className="font-normal hover:font-bold items-center text-lg text-gray-200 hover:underline underline-offset-[4px] decoration-[1px] hover:text-white md:border-r-[2px] border-r-gray-300 hoverEffect last:border-r-0" key={item._id}>
+
+                       </li>   
+                    ))}
+                </ul>
+                </div>
+                </motion.div>
+                </div>
+            )}  
+        </div>
         </Flex>
         </nav>
         </div>
