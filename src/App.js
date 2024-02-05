0e11835e1059be854fda2257a424e6bf31d164d1
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-05 22:09:50 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import Header from "./components/Header/Header";
 
 
-const Layout = () => {
-  return (
-   <div>
-    <Header />
-   </div>
-  );
-};
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-     <Route path="/" element={<Layout />}></Route>
-    </Route>
-  )
-);
-
-function App() {
-  return (
-    <div className="font-bodyFont">
-      <RouterProvider router={router} />
-    </div>
-  );
-}
-
-export default App;
