b9df0969d953a12e2ca815836e62cf720c719401
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-06 23:48:53 +0300

    redux

---
+++ src/reducers/index.js
@@ -0,0 +1,58 @@
+import { combineReducers } from 'redux'
+import { GET_ALL_PRODUCT,GET_NUMBER_CART,ADD_CART,DECREASE_QUANTITY,INCREASE_QUANTITY,DELETE_CART } from '../actions/index'};
+
+const initProduct = {
+    numberCart:0,
+    Carts:[],
+    _products:[]
+}
+
+function todoProduct(state = initProduct,action){
+    switch(action.type){
+        case GET_ALL_PRODUCT:
+            return{
+                ...state,
+                _products:action.payload
+            }
+
+            case GET_NUMBER_CART:
+                return{
+                    ...state
+                }
+
+                case ADD_CART:
+                    if(state.numberCart == 0){
+                        let cart = {
+                            id:action.payload.id,
+                            quantity:1,
+                            name:action.payload.name,
+                            image:action.payload.image,
+                            price:action.payload.price
+                        }
+                        state.Carts.push(cart);
+                    } else {
+                        let check = false;
+                        state.Carts.map((item,key)=>{
+                            if(item.id==action.payload.id){
+                                state.Carts[key].quantity++;
+                                check=true;
+                            }
+                        });
+                        if(!check){
+                            let _cart = {
+                                id:action.payload.id,
+                                quantity:1,
+                                name:action.payload.name,
+                                image:action.payload.image,
+                                price:action.payload.price
+                            }
+                            state.Carts.push(_cart);
+                        }
+                    }
+
+                    return{
+                        ...state,
+                        numberCart:state.numberCart+1
+                    }
+    }
+}
