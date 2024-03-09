b67c350d327fa2802d6983baaeacd1ac01a3a5e1
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-09 22:43:04 +0300

    started new project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import  Header  from "./components/Header/index";
 import React from "react";
-import { BrowserRouter, Routes,Link,Route,Switch,  } from "react-router-dom"; 
-import Products from './components/Product/Products';
+import Header from "./components/Header";
 
 
 
 function App() {
     return (
-        <BrowserRouter>
-        <Header/>
-        <Routes>
-       <Route path="products" element={<Products />}></Route>
-        </Routes>
-        </BrowserRouter>
+        <Header />
     )
 }
-
+   
 export default App;
--- src/stores/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from 'redux-thunk'
-import ShopApp from "../reducers";
-
-
-
-const store = createStore(ShopApp,applyMiddleware(thunk));
-export default store
