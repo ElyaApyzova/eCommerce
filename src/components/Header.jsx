d105e7b7ea158ac087482d41cd0502983d9c1d84
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-10 22:00:17 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
     return (
-     <div>
-        
+     <div className="header">
+        <div className="container">
+            <div className="inner__header">
+                <nav className="menu">
+                 <ul className="menu__list">
+                    <li className="list__item">
+                        <Link>Home</Link>
+                    </li>
+                    <li className="list__item">
+                        <Link>Product</Link>
+                    </li>
+                    <li className="list__item">
+                        <Link>Contact</Link>
+                    </li>
+                 </ul>
+                </nav>
+            </div>
+            </div>
      </div>
     )
 }
